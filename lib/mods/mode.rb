c3c0e2ac007f5e3bcd194d845bf76d146a3af06f
Alex Coreman <scorpionsik@gmail.com>
2021-08-12 01:47:27 +0300

    Add edit message feature

--- lib/mods/mode.rb
+++ lib/mods/mode.rb
@@ -4,8 +4,13 @@ class AnimaliaBot
 			def show_view(view)
 				view.last.nil? ? Listener::Response.std_message(view.first) : Listener::Response.inline_message(view.first, view.last)
 			end
+			
+			def update_view(message, view)
+				view.last.nil? ? Listener::Response.edit_std_message(view.first, message) : Listener::Response.edit_inline_message(view.first, view.last, message)
+			end
 			module_function(
-				:show_view
+				:show_view,
+				:update_view
 			)
 		end
 	end
--- lib/response.rb
+++ lib/response.rb
@@ -1,26 +1,56 @@
 class AnimaliaBot
 	module Listener
 		module Response
-			def std_message(message, chat_id = false)
+			def std_message(text, chat_id = false)
 				chat = Listener.village.telegram_chat
 				chat = chat_id if chat_id
 				Listener.bot.api.send_message(
 					parse_mode: 'html',
 					chat_id: chat,
-					text: message
+					text: text
 				)
 			end
 			
-			def inline_message(message, inline_markup, editless = false, chat_id = false)
+			def inline_message(text, inline_markup, chat_id = false)
 				chat = Listener.village.telegram_chat
 				chat = chat_id if chat_id
 				#puts chat
-				#puts "#{chat}: #{message}\r\n#{inline_markup}"
+				#puts "#{chat}: #{text}\r\n#{inline_markup}"
 				Listener.bot.api.send_message(
 					chat_id: chat,
 					parse_mode: 'html',
-					text: message,
+					text: text,
 					reply_markup: inline_markup
+				)
+			end
+			
+			def edit_std_message(new_text, message = false, chat_id = false)
+				message_id = Listener.message.message_id
+				message_id = message.message_id if message
+				
+				chat = Listener.village.telegram_chat
+				chat = chat_id if chat_id
+				Listener.bot.api.edit_message_text(
+					chat_id: chat, 
+					message_id: message_id, 
+					parse_mode: 'html',
+					text: new_text,
+					)
+			end
+			
+			def edit_inline_message(new_text, new_inline_markup, message = false, chat_id = false)
+				message_id = Listener.message.message.message_id
+				message_id = message.message_id if message
+				
+				chat = Listener.village.telegram_chat
+				chat = chat_id if chat_id
+				
+				Listener.bot.api.edit_message_text(
+					chat_id: chat,
+					message_id: message_id, 
+					parse_mode: 'html',
+					text: new_text,
+					reply_markup: new_inline_markup
 				)
 			end
 			
@@ -33,6 +63,8 @@ class AnimaliaBot
 			module_function(
 				:std_message,
 				:inline_message,
+				:edit_std_message,
+				:edit_inline_message,
 				:generate_inline_markup
 			)
 		end
