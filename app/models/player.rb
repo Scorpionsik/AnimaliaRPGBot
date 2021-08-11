e8fb2125630c5aab6106e9616f3e86701f42b68d
Alex Coreman <scorpionsik@gmail.com>
2021-08-11 11:58:55 +0300

    Update Activerecord associations to set current village for player

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,10 +2,10 @@ require 'active_record'
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/bot.db')
 
 class Player < ActiveRecord::Base
-	has_many :villages
+	belongs_to :village
 	has_many :animals
 	
 	def get_select_animal
-		animals.find_by(id: self.select_animal, player_id: self.id, village_id: self.village_id)
+		self.animals.find_by(id: self.select_animal)
 	end
 end
